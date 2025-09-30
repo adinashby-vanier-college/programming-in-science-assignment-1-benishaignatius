f637d245c8a519d1b226e7795bfde0c8be9726b7
Benisha Ignatius <benishaignatius@gmail.com>
2025-09-30 09:37:41 -0400

    Solved assignment 1

--- Assignment1.py
+++ Assignment1.py
@@ -25,17 +25,33 @@ def check_number(number):
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
 def star_shape(rows):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = ""
+
+    for i in range(1, rows+1): 
+        for j in range(i):
+            result += "*"
+        result = result.rstrip()
+        result += "\n"
+
+    return result.rstrip()
+
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
 def count_multiples_of_3(limit):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = ""
+    for limit in range(1, limit + 1):
+        if limit % 3 == 0:
+            result += "Multiple of 3\n"
+        else:
+            result += str(limit) + "\n"
+    return result.rstrip()
 
 # Function 6: Sum of Even Numbers in a Range
 # This function should calculate and return the sum of even numbers within a given range.
 def sum_of_even_numbers(start, end):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = 0
+    for number in range(start, end + 1):
+        if number % 2 == 0:
+            result += number
+    return result
